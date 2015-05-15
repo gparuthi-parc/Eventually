4c14e30aa69df6931335c8b78a51750b8a35df74
David Barsky <barskydavid@gmail.com>
2015-05-15 19:18:44 -0400

    Initial Commit

---
+++ Almanac/Controllers/AppDelegate.swift
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.swift
+//  Almanac
+//
+//  Created by David Barsky on 5/15/15
+//  Copyright (c) 2015 David Barsky. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+}
