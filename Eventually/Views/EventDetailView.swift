02e6e32687eb91a6bc3f9f221689d50d2b2980e0
David Barsky <barskydavid@gmail.com>
2015-06-02 17:02:15 -0400

    effin autolayout

--- Eventually/Views/EventDetailView.swift
+++ Eventually/Views/EventDetailView.swift
@@ -18,32 +18,43 @@ class EventDetailView: UIView {
     let titleLabel = UILabel()
     let dayLabel = UILabel()
     let timeLabel = UILabel()
-    
     let locationLabel = UILabel()
     let attendeesLabel = UILabel()
     
     // grouping views
-    
     let titleView = UIView()
     
     override func layoutSubviews() {
         super.layoutSubviews()
-        
-        println("things are called")
-        
         self.backgroundColor = UIColor.headerBackgroundColor()
-        self.addLabelToView(titleLabel, fontName: UIFont.appFont(.Light), fontSize: 24, textColor: UIColor.primaryTextColor())
+
+        self.addSubview(titleView)
+        titleView.backgroundColor = UIColor.whiteColor()
+        self.addInformationLabels(titleView)
+    }
+    
+    // not a particullarly reuseable function, but still useful for grouping
+    func addInformationLabels(view: UIView) {
+        view.addLabelToView(titleLabel, fontName: UIFont.appFont(.Light), fontSize: 24, textColor: UIColor.primaryTextColor())
         self.titleLabel.text = "This is an Event"
         
-        self.addLabelToView(dayLabel, fontName: UIFont.appFont(.Regular), fontSize: 16, textColor: UIColor.primaryTextColor())
+        view.addLabelToView(dayLabel, fontName: UIFont.appFont(.Light), fontSize: 16, textColor: UIColor.primaryTextColor())
         self.dayLabel.text = "Tuesday, August 25"
-
-        // labels in TitleView
+        
+        view.addLabelToView(locationLabel, fontName: UIFont.appFont(.Light), fontSize: 16, textColor: UIColor.primaryTextColor())
+        self.locationLabel.text = "101 Arch Stree, Boston"
+        
         constrain(titleLabel, dayLabel, locationLabel) { titleLabel, dayLabel, locationLabel in
             let superview = titleLabel.superview!
 
             titleLabel.leadingMargin == superview.leadingMargin + 24
             titleLabel.centerYWithinMargins == superview.topMargin + 24
+            
+            dayLabel.leadingMargin == superview.leadingMargin + 24
+            dayLabel.topMargin == titleLabel.bottomMargin
+            
+            locationLabel.leadingMargin == superview.leadingMargin + 24
+            locationLabel.topMargin == dayLabel.bottomMargin + 5
         }
     }
 }
